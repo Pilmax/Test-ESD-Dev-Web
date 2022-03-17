be937753c58645dd15dc95a324a984c29f43c802
paule juoilde <guillaumemengelatte@hotmail.fr>
2022-03-17 13:49:30 +0100

    Temp version

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let response = []
 
+    console.log(document.querySelector('form'));
 let button = document.getElementById('val').onclick = () => {
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
     $('#serialize').text(        
         response = JSON.stringify($('form').serialize())
-    );
-    document.cookie = response
+        );
+        console.log(response);
+    if(response == "1=true&2=true&3=true&4=true&5=true"){
+        console.log('validé');
+    }else{
+        console.log("nope");
+    }
+    
+    //document.cookie = response
     // console.log(JSON.stringify($('form').serialize()));
-    console.log(response);
     window.location.href = './Validation.html'
 };
+
+// To create a Cookie:
+// Cookies.set('name', 'value');
+
+// To read a Cookie:
+// Cookies.get('name'); // => 'value'
